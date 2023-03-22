ee314d9e9b1878c0c7c81648fb2afb1fcafb11b4
JelleHoorne <jelle.hoorne@skyline.be>
2023-03-22 10:59:02 +0100

    Add project files.

---
+++ EmpowerTrialRun1_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerTrialRun1_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerTrialRun1_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("D73C2A45-7980-4BDC-A50D-9BC47402EF2C")]
+[assembly: AssemblyVersion("1.0.0.0")]
